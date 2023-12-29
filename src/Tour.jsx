1106d91b0464880de2718a456ce931b404c29bb2
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-29 19:48:42 +0530

    Add Read More Functionality

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
+
 const Tour = (props) => {
   const { tour_data, removeTour } = props;
   const { id, info, image, name, price } = tour_data;
+
+  const [readMore, setReadMore] = useState(false);
+  console.log(info.substring(0, 12));
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
@@ -8,7 +14,17 @@ const Tour = (props) => {
       <div className="tour-info">
         <h3>{name}</h3>
 
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 179)}...`}
+
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => setReadMore(!readMore)}
+          >
+            {readMore ? 'show less' : 'read more'}
+          </button>
+        </p>
         <button
           type="btn"
           className="btn btn-block delete-btn"
